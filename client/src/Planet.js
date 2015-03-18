52b8ef0aabb7fb864c9d66e556240f30509b21d6
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-03-18 05:18:46 +0000

    Updated the Planet class. Added orbits, and the planets are no longer spheres

--- client/src/Planet.js
+++ client/src/Planet.js
@@ -4,19 +4,53 @@
 
 /**
  * The Planet Class, returns a planet
- * @method
+ * @class
  */
-var Planet = function(size, posx, posy, posz, texture, clouds){
+var Planet = function(prop){
+	log.debug(prop.name + " Initialized");
+
+	var texture = "";
 
 	var pMesh = new THREE.SphereGeometry( 1, 100, 100);
 
+	var planetMesh = new THREE.BoxGeometry(1,1,1, 100,100,100 );
+
+	for (var i = planetMesh.vertices.length - 1; i >= 0; i--) {
+		planetMesh.vertices[i].normalize().multiplyScalar(prop.radius);
+	}
+
 	var pTexture = new THREE.ImageUtils.loadTexture( texture );
 
-  var pMaterial = new THREE.MeshLambertMaterial({map: pTexture});
-	var pObj = new THREE.Mesh( pMesh, pMaterial );
+	var pMaterial = new THREE.MeshLambertMaterial();
+	//var pMaterial = new SolarShader.planetShader();
 
-	pObj.position.set(posx, posy, posz);
-	pObj.scale.set(size*2, size*2, size*2);
+	this.pObj = new THREE.Mesh( planetMesh, pMaterial );
 
-	return pObj;
+	//this.pObj.scale.set(prop.radius*2, prop.radius*2, prop.radius*2);
+
+	scene.add(this.pObj);
+
+	this.setDistance(prop.distance);
+	this.setOrbitTime(prop.distance);
+
+	this.update();
+
+};
+
+Planet.prototype.setDistance = function(distance){
+	this.distance = distance;
+};
+
+Planet.prototype.setOrbitTime = function(otime){
+	// Orbit time is provided in seconds
+	this.orbitTime = otime*1000;
+};
+
+Planet.prototype.update = function( delta ){
+	this.pObj.position.x = this.distance * Math.cos( ( (new Date()).getTime() / this.orbitTime ));
+	this.pObj.position.z = this.distance * Math.sin( ( (new Date()).getTime() / this.orbitTime ));
+};
+
+Planet.prototype.getObject = function(){
+	return this.pObj;
 };
