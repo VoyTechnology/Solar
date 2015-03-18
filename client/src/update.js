d44c4e38f1fc9b4bf1c0eb52d6b8210bbfca9df7
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-03-18 05:14:33 +0000

    Updated the render update logic

--- client/src/update.js
+++ client/src/update.js
@@ -6,28 +6,49 @@
 var myLastPos;
 var myLastDir;
 
+var myPos, myDir;
+
 var frameCount = 0;
 
+
+
 function update(){
+  var delta = clock.getDelta();
+  player.update( delta );
   frameCount++;
 
+  myPos = {
+    x: player.camera.position.x,
+    y: player.camera.position.y,
+    z: player.camera.position.z,
+  };
+
+
+  myDir = {
+    x: player.camera.rotation.x,
+    y: player.camera.rotation.y,
+    z: player.camera.rotation.z,
+  };
+
+  // Do actions every 60 frames
   if(frameCount % 60 === 0){
-    // Do stuff every second
+
   }
 
-  var delta = clock.getDelta();
-
-  var myPos = camera.position;
-  var myDir = camera.rotation;
-
-  if( (myPos != myLastPos) || (myDir != myLastDir) ){
-    connection.updateLocation(myPos, myDir);
+  // Do actions every 3 frames (20 times a second)
+  if(frameCount % 3 === 0){
+    if( (myPos != myLastPos) || (myDir != myLastDir) ){
+      connection.updateLocation(myPos, myDir);
+    }
   }
+
+  // Update each planet
+  for(var i = 0; i < planets.length; i++){
+     planets[i].update( delta );
+  }
+
+
 
   myLastPos = myPos;
   myLastDir = myDir;
-
-  //controls.movementSpeed = 0.33 * 10;
-  controls.update( delta );
-  controls.dragTolook = false;
 }
