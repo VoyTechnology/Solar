f9b8a4e29915ad669b49e8583b85d49944a4d7b9
Mladen Kajic <mladen.kajic2@mail.dcu.ie>
2015-03-18 06:04:45 +0000

    Upgraded dummy data init.

--- servers/database/dummy_data/dummyDataInit.js
+++ servers/database/dummy_data/dummyDataInit.js
@@ -1,6 +1,7 @@
 /*
 This file is used to initialise the database
 */
+console.log("Please Wait");
 
 // loading config file
 var config = require(__dirname + "/config.json");
@@ -18,92 +19,97 @@ if (config.deleteFirst) {
     db.players.remove({});
 }
 
-// Creating special accounts
-var jamesData = {
-    // authentication
-    auth : {
-        id : 0,
-        token : "0",
-        email : "example@gmail.com",
-        username : "james",
-        password : "windows4life",
-    },
-    // player
-    player : {
-        id : 0,
-        username : "james",
-        ship : "astratis_v1",
-        position : config.startingPosition,
-        orientation : {x:0,y:0,z:0},
-    }
-};
-db.authentication.insert(jamesData.auth);
-db.players.insert(jamesData.player);
-
-var wojData = {
-    // authentication
-    auth : {
-        id : 1,
-        token : "1",
-        email : "example@gmail.com",
-        username : "woj",
-        password : "windows4life",
-    },
-    // player
-    player : {
-        id : 1,
-        username : "woj",
-        ship : "astratis_v1",
-        position : config.startingPosition,
-        orientation : {x:0,y:0,z:0},
-    }
-};
-db.authentication.insert(wojData.auth);
-db.players.insert(wojData.player);
-
-var mladenData = {
-    // authentication
-    auth : {
-        id : 2,
-        token : "2",
-        email : "example@gmail.com",
-        username : "mladen",
-        password : "windows4life",
-    },
-    // player
-    player : {
-        id : 2,
-        username : "mladen",
-        ship : "astratis_v1",
-        position : config.startingPosition,
-        orientation : {x:0,y:0,z:0},
-    }
-};
-db.authentication.insert(mladenData.auth);
-db.players.insert(mladenData.player);
-
-// looping through test accounts to make
-// starting token is the amount of special accounts
-for (i=config.numSpecialAccounts; i<(config.numTestAccounts + config.numSpecialAccounts); i++) {
-    // creating document for AUTHENTICATION collection
-    var authenticationData = {
-        id : i,
-        token : i.toString(),
-        email : "example@gmail.com",
-        username : config.entryNamePattern + i.toString(),
-        password : "CoolPass",
+// wait for database before creating entrys
+setTimeout(function() {
+    // Creating special accounts
+    var jamesData = {
+        // authentication
+        auth : {
+            id : 0,
+            token : "0",
+            email : "example@gmail.com",
+            username : "james",
+            password : "windows4life",
+        },
+        // player
+        player : {
+            id : 0,
+            username : "james",
+            ship : "astratis_v1",
+            position : config.startingPosition,
+            orientation : {x:0,y:0,z:0},
+        }
     };
+    db.authentication.insert(jamesData.auth);
+    db.players.insert(jamesData.player);
 
-    // creating document for PLAYERS collection
-    var playerData = {
-        id : i,
-        username : config.entryNamePattern + i.toString(),
-        ship : "astratis_v1",
-        position : config.startingPosition,
-        orientation : {x:0,y:0,z:0},
+    var wojData = {
+        // authentication
+        auth : {
+            id : 1,
+            token : "1",
+            email : "example@gmail.com",
+            username : "woj",
+            password : "windows4life",
+        },
+        // player
+        player : {
+            id : 1,
+            username : "woj",
+            ship : "astratis_v1",
+            position : config.startingPosition,
+            orientation : {x:0,y:0,z:0},
+        }
     };
+    db.authentication.insert(wojData.auth);
+    db.players.insert(wojData.player);
 
-    // uploading documents to server
-    db.authentication.insert(authenticationData);
-    db.players.insert(playerData);
-}
+    var mladenData = {
+        // authentication
+        auth : {
+            id : 2,
+            token : "2",
+            email : "example@gmail.com",
+            username : "mladen",
+            password : "windows4life",
+        },
+        // player
+        player : {
+            id : 2,
+            username : "mladen",
+            ship : "astratis_v1",
+            position : config.startingPosition,
+            orientation : {x:0,y:0,z:0},
+        }
+    };
+    db.authentication.insert(mladenData.auth);
+    db.players.insert(mladenData.player);
+
+    // looping through test accounts to make
+    // starting token is the amount of special accounts
+    for (i=config.numSpecialAccounts; i<(config.numTestAccounts + config.numSpecialAccounts); i++) {
+        // creating document for AUTHENTICATION collection
+        var authenticationData = {
+            id : i,
+            token : i.toString(),
+            email : "example@gmail.com",
+            username : config.entryNamePattern + i.toString(),
+            password : "CoolPass",
+        };
+
+        // creating document for PLAYERS collection
+        var playerData = {
+            id : i,
+            username : config.entryNamePattern + i.toString(),
+            ship : "astratis_v1",
+            position : config.startingPosition,
+            orientation : {x:0,y:0,z:0},
+        };
+
+        // uploading documents to server
+        db.authentication.insert(authenticationData);
+        db.players.insert(playerData);
+    }
+
+    console.log("Done");
+}, 2000);
