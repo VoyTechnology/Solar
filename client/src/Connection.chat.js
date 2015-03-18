7112ca4eba169e20f7b9f4e47261d66418f38ba7
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-03-18 18:14:53 +0000

    Moved James' chat to the main Connection.js, After compressing there seemed to be some issues with inproper prototyping etc

--- client/src/Connection.chat.js
+++ client/src/Connection.chat.js
@@ -1,16 +1,16 @@
-Connection.prototype.sendMessage = function (recipients, msg) {
-
-    var that = this;
-
-    if (that.authorised) {
-
-        var messageData = {
-            timestamp: (new Date()).getTime(),
-            originator: +settings.get('username'),
-            recipient: recipients,
-            text: msg
-        };
-
-        that._socket.emit('chat', messageData);
-    }
-};
+// Connection.prototype.sendMessage = function (recipients, msg) {
+//
+//     var that = this;
+//
+//     if (that.authorised) {
+//
+//         var messageData = {
+//             timestamp: (new Date()).getTime(),
+//             originator: +settings.get('username'),
+//             recipient: recipients,
+//             text: msg
+//         };
+//
+//         that._socket.emit('chat', messageData);
+//     }
+// };
--- client/src/Connection.js
+++ client/src/Connection.js
@@ -143,3 +143,21 @@ Connection.prototype.disconnect = function(msg){
     'reasonText': msg
   });
 };
+
+
+Connection.prototype.sendMessage = function (recipients, msg) {
+
+    var that = this;
+
+    if (that.authorised) {
+
+        var messageData = {
+            timestamp: (new Date()).getTime(),
+            originator: +settings.get('username'),
+            recipient: recipients,
+            text: msg
+        };
+
+        that._socket.emit('chat', messageData);
+    }
+};
