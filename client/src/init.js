34d0a9447136e1743ab4fb5e841eae717b703dda
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-03-18 05:22:50 +0000

    Changed order of initialization, changed the default camera values to facilitate the new player object

--- client/src/init.js
+++ client/src/init.js
@@ -5,23 +5,38 @@
 function init(){
 
   // Assign values to their corresponding definitions
-  settings  = new Settings();
-  log       = new Logger("Logger initialized");
-  gonsole   = new Gonsole();
-  connection =  new Connection();
+  settings  =   new Settings(config.defaults);
+  log       =   new Logger("Logger initialized");
+  connection=   new Connection();
+  gonsole   =   new Gonsole();
+  player    =   new Player();
+
+  // setTimeout( function(){
+  //   var connected_event = new Event('server_connected');
+  //   connection.established = true;
+  // }, 1000);
+
+  document.addEventListener('server_connected', hide_loading_window());
+
+  // Try to connect and then wait to see was connection successful
+  connection.connect();
+  connection.otherPlayers();
+
+
+
+  setTimeout(function(){
+
+    if(!connection.established){
+      game_exit_error(802);
+    } else {
+      connection.updateOthers();
+    }
+  // Wait until displaying the error message
+  },config.connectionTimeout);
+
 
   scene = new THREE.Scene();
   clock = new THREE.Clock();
-
-  // Create a new camera and cast the return values from the settings to ints
-  camera = new THREE.PerspectiveCamera(
-    +settings.get('field_of_view'),
-    +settings.get('screen_aspect_ratio'),
-    +settings.get('min_render_distance'),
-    +settings.get('max_render_distance')
-  );
-
-  controls = new THREE.FlyControls( camera );
 
   renderer = new THREE.WebGLRenderer({
     logarithmicDepthBuffer: true,
@@ -32,12 +47,14 @@ function init(){
 
   document.body.appendChild( renderer.domElement );
 
+  //player.loadShip( 'Astratis' );
+
   // Temporary values
-  controls.movementSpeed = 10;
-  controls.domElement = renderer.domElement ;
-  controls.rollSpeed = Math.PI / 15;
-  controls.autoForward = false;
-  controls.dragTolook = false;
+  player.controls.movementSpeed = 1000;
+  player.controls.domElement = renderer.domElement ;
+  player.controls.rollSpeed = Math.PI / 15;
+  player.controls.autoForward = false;
+  player.controls.dragTolook = false;
 
   // Initialize the world
   world();
