7e3512f5dd43f4d223412d3c621476ed3daf40ea
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-03-18 06:50:32 +0000

    Updated the discovery server to load from config file

--- servers/discovery-server/discovery-server.go
+++ servers/discovery-server/discovery-server.go
@@ -1,56 +1,18 @@
 package main
 
-// Import essential components
 import (
 	"net/http"
-	"encoding/json"
+	"io/ioutil"
 )
 
-// Struct to store the list of addresses
-type AddressesMessage struct {
-	AuthServer 						string				`json:"authServer"`
-	GameServer 						string				`json:"gameServer"`
-	TradeServer 					string				`json:"tradeServer"`
-}
+var file, err = ioutil.ReadFile("./config.json")
 
-// Struct to store the complete message
-type CompleteMessage struct {
-	Success 						bool				`json:"success"`
-	Addresses 						AddressesMessage	`json:"addresses"`
-}
-
-type FailedMessage struct {
-	Success 						bool				`json:"success"`
-}
-
-// Declare and fill the messages
-var (
-	addrMsg AddressesMessage 	= AddressesMessage{ "http://178.62.116.176:3001/", "http://178.62.116.176:3000/", "http://178.62.116.176:3002/" }
-	failMsg FailedMessage		= FailedMessage{ false }
-	compMsg CompleteMessage 	= CompleteMessage{ true, addrMsg }
-	m []byte					= getMessage( compMsg, failMsg )
-)
-
-// Produces the complete ready to send message from the complete message
-func getMessage(cm CompleteMessage, fm FailedMessage) []byte {
-	msg, err 	:= json.Marshal( cm )
-	fmsg, ferr 	:= json.Marshal( fm )
-
-	if err == nil && ferr == nil{
-		return msg
-	}
-
-	return fmsg
-}
-
-// getServers function handling the request
 func getServers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Write( m )
+	w.Write( file )
 }
 
-// Main function
 func main() {
-    http.HandleFunc("/", getServers )
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/getServers", getServers )
+	http.ListenAndServe(":3003", nil)
 }
