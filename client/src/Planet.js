1c6d385cddf2a7204acdc898f1bc206a1d7f5941
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-03-31 01:56:51 +0100

    Updated to use chunk positioning system. Added basic random procedural generation (very very basic). Fixes issue #30 - the planet shading

--- client/src/Planet.js
+++ client/src/Planet.js
@@ -6,44 +6,51 @@
  * The Planet Class, returns a planet
  * @class
  */
-var Planet = function(prop){
-	log.debug(prop.name + " Initialized");
+function Planet( properties ){
+	this.properties = properties;
 
-	var texture = "";
+	log.debug(this.properties.name + " Initialized");
 
-	var pMesh = new THREE.SphereGeometry( 1, 100, 100);
 
-	var planetMesh = new THREE.BoxGeometry(1,1,1, 100,100,100 );
+	this.distance = this.properties.distance;
 
-	for (var i = planetMesh.vertices.length - 1; i >= 0; i--) {
-		planetMesh.vertices[i].normalize().multiplyScalar(prop.radius);
+	var radius = this.properties.radius;
+
+	// Create the 3D geometry
+	var planetMesh = new THREE.BoxGeometry(1,1,1, 80,80,80 );
+
+
+	if(this.properties.type == "gas" || this.properties.type == "water"){
+
+		for (var vf = planetMesh.vertices.length - 1; vf >= 0; vf--) {
+			planetMesh.vertices[vf].normalize().multiplyScalar(radius);
+		}
+
+	} else {
+
+		for (var vr = planetMesh.vertices.length - 1; vr >= 0; vr--) {
+			planetMesh.vertices[vr].normalize().multiplyScalar(radius - Math.random()*(radius/100));
+		}
+
 	}
 
-	var pTexture = new THREE.ImageUtils.loadTexture( texture );
+	planetMesh.computeFaceNormals();
+	planetMesh.computeVertexNormals();
 
-	var pMaterial = new THREE.MeshLambertMaterial();
-	//var pMaterial = new SolarShader.planetShader();
 
-	this.pObj = new THREE.Mesh( planetMesh, pMaterial );
+	var planetMaterial = new THREE.MeshLambertMaterial({
+		color: this.resolveColour(),
+		wireframe: settings.get('wireframe') == 'true'
+	});
+	this.planetObject = new THREE.Mesh( planetMesh, planetMaterial );
 
-	//this.pObj.scale.set(prop.radius*2, prop.radius*2, prop.radius*2);
+	scene.add(this.planetObject);
 
-	scene.add(this.pObj);
+	this.setOrbitTime(this.properties.distance);
 
-	this.setDistance(prop.distance);
-	this.setOrbitTime(prop.distance);
+	//this.update();
+}
 
-	this.update();
-
-};
-
-/**
- * Sets the distance away from the sun
- * @method
- */
-Planet.prototype.setDistance = function(distance){
-	this.distance = distance;
-};
 
 /**
  * Sets the orbit time
@@ -59,15 +66,33 @@ Planet.prototype.setOrbitTime = function(otime){
  * @method
  */
 Planet.prototype.update = function( delta ){
-	this.pObj.position.x = this.distance * Math.cos( ( (new Date()).getTime() / this.orbitTime ));
-	this.pObj.position.z = this.distance * Math.sin( ( (new Date()).getTime() / this.orbitTime ));
+	this.planetObject.position.set(
+		(player.chunk.x * -1000) + (this.distance * Math.cos( Date.now() / this.orbitTime )),
+		(player.chunk.y * -1000),
+		(player.chunk.z * -1000) + (this.distance * Math.sin( Date.now() / this.orbitTime ))
+	);
 };
 
 /**
  * Gets the planet object
  * @method
- * @returns {Object} THREE.js object 
+ * @returns {Object} THREE.js object
  */
 Planet.prototype.getObject = function(){
-	return this.pObj;
+	return this.planetObject;
+};
+
+Planet.prototype.resolveColour = function(){
+	switch(this.properties.type){
+		case 'rock':
+			return 0x3E2723;
+		case 'metal':
+			return 0x424242;
+		case 'green':
+			return 0x2E7D32;
+		case 'water':
+			return 0x1976D2;
+		case 'gas':
+			return 0xCDDC39;
+	}
 };
