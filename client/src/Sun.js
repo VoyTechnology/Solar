c77dc6a4d16979a80cf5fcbc8d326ef91b06ecb6
Wojciech Bednarzak <wojtek.bednarzak@gmail.com>
2015-03-31 01:57:28 +0100

    Added support for chunk positioning system

--- client/src/Sun.js
+++ client/src/Sun.js
@@ -18,20 +18,27 @@ var Sun = function( size, color, lumcolor, intensity, range ){
 	this.sunObj.position.set(0,0,0);
 	this.sunObj.scale.set(size*2, size*2, size*2);
 
-	var sunLight = new THREE.PointLight( lumcolor, intensity, range );
-	sunLight.position.set(0,0,0);
-	sunLight.castShadow = true;
-	sunLight.shadowCameraNear = 10;
-	sunLight.shadowCameraFar = Math.pow(2,53)-1;
-	sunLight.shadowCameraFov = 10;
-	sunLight.shadowBias = -0.00021;
-	sunLight.shadowDarkness = 0.9;
-	sunLight.shadowMapWidth = 4096;
-	sunLight.shadowMapHeight = 4096;
-	sunLight.shadowCameraVisible = true;
-	sunLight.target = camera;
-	scene.add(sunLight);
+	this.sunLight = new THREE.PointLight( lumcolor, intensity, range );
+	this.sunLight.position.set(0,0,0);
+	this.sunLight.castShadow = true;
+	this.sunLight.shadowCameraNear = 10;
+	this.sunLight.shadowCameraFar = player.camera.far;
+	this.sunLight.shadowCameraFov = player.camera.fov;
+	this.sunLight.shadowBias = -0.00021;
+	this.sunLight.shadowDarkness = 0.9;
+	this.sunLight.shadowMapWidth = 4096;
+	this.sunLight.shadowMapHeight = 4096;
+	this.sunLight.shadowCameraVisible = true;
+	scene.add(this.sunLight);
 
 	scene.add(this.sunObj);
-	return this.sObj;
+};
+
+Sun.prototype.update = function(){
+	this.sunObj.position.set(player.chunk.x * -1000, player.chunk.y * -1000, player.chunk.z * -1000);
+	this.sunLight.position.set(
+		this.sunObj.position.x,
+		this.sunObj.position.y,
+		this.sunObj.position.z
+	);
 };
